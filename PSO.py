55c6cfe1aae1f51c88b6d550950c332d2ef6d309
CarloLonghi <carlo.longhi98@gmail.com>
2021-07-26 12:08:35 +0200

    final planner

--- PSO.py
+++ PSO.py
@@ -1,11 +1,7 @@
-import math
-from sys import path
-from typing import List
 from matplotlib.lines import Line2D
 import numpy.random as rnd
 import numpy as np 
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
 from math import sqrt
 import PathVisualization as PV
 
@@ -64,59 +60,62 @@ def getRandomInt(bounds):
 # f = fitting function
 # bounds = bounds of the search space
 # n = number of particles
-# obstcales map of the environment
-def initialization(f,bounds,n, map):
-    paths = np.zeros((n,len(bounds)))
+# map = obstcales map of the environment
+# map_dim = length of the map border
+# num_points = number of points forming a path
+def initialization(f,bounds,n, map, map_dim, num_points):
+    paths = np.zeros((n,num_points))
     paths = paths.tolist()
     particle_val = paths[:]
     velocity = paths[:]
     local_best = paths[:]
     local_best_val = paths[:]
     
-    global_best = [[0,map_dimension-1],[0,0],[0,map_dimension-1],[0,0]]
+    global_best = []
+    global_best_val = map_dim * num_points
     for p in range(n):  # generate random points for a path
         reset = True
 
         while reset:
             reset = False
 
-            paths[p] = [getRandomInt(bounds[i]) for i in range(len(bounds))]
+            paths[p] = [getRandomInt(bounds[i]) for i in range(num_points)]
             paths[p] = [i.astype(int) for i in paths[p]]
 
             # check if the path crosses an obstacle and generate new random points
-            num_tries = 0
-            while (not checkSegment(np.array([0,0]),paths[p][0]) or map[paths[p][0][0],paths[p][0][1]] == 255) and num_tries<20:
-                num_tries += 1
+            while (not checkSegment(np.array([0,0]),paths[p][0]) or map[paths[p][0][0],paths[p][0][1]] == 255):
                 paths[p][0] = getRandomInt(bounds[0])
 
-            if num_tries >= 50:
-                reset = True
-
-
-            for i in range(1,len(bounds)-1):
+            i = 1
+            while i < num_points-1 and not reset:
                 num_tries = 0
                 while (not checkSegment(paths[p][i-1],paths[p][i]) or map[paths[p][i][0],paths[p][i][1]] == 255) and num_tries<20:
                     num_tries += 1
                     paths[p][i] = getRandomInt(bounds[i])
                 
-                if num_tries >= 50:
+                if num_tries >= 20:
                     reset = True
+                
+                i += 1
 
-            num_tries = 0
-            while (not checkSegment(paths[p][-1],np.array([map_dimension-1,map_dimension-1])) or \
-                not checkSegment(paths[p][-2],paths[p][-1]) or map[paths[p][-1][0],paths[p][-1][1]] == 255) and num_tries<20:
-                paths[p][-1] = getRandomInt(bounds[-1])
-            
-            if num_tries >= 50:
+            if not reset:
+                num_tries = 0
+                while (not checkSegment(paths[p][-1],np.array([map_dimension-1,map_dimension-1])) or \
+                    not checkSegment(paths[p][-2],paths[p][-1]) or map[paths[p][-1][0],paths[p][-1][1]] == 255) and num_tries<20:
+                    num_tries += 1
+                    paths[p][-1] = getRandomInt(bounds[-1])
+                
+                if num_tries >= 20:
                     reset = True
 
 
         particle_val[p] = f(paths[p])
-        velocity[p] = [rnd.uniform(-abs(bounds[i][1]-bounds[i][0]),abs(bounds[i][1]-bounds[i][0]),2) for i in range(len(bounds))]
+        velocity[p] = [rnd.uniform(-abs(bounds[i][1]-bounds[i][0]),abs(bounds[i][1]-bounds[i][0]),2) for i in range(num_points)]
         local_best[p] = paths[p]
         local_best_val[p] = particle_val[p]
-        if(local_best_val[p] < f(global_best)):
+        if(local_best_val[p] < global_best_val):
             global_best = local_best[p]
+            global_best_val = f(global_best)
 
 
     return np.array(paths), np.array(particle_val), np.array(velocity), np.array(local_best), global_best, np.array(local_best_val)
@@ -124,18 +123,22 @@ def initialization(f,bounds,n, map):
 # function that implements the PSO algorithm
 # f = fitting function
 # bounds = bounds of the search space
-# phip,phig,omega = pso parameter
+# phip,phig,omega = pso parameters
 # n = number of particles
-# obstcales map of the environment
+# map = obstcales map of the environment
 # tol = precision of the pso algorithm
-def particle_swarm_optimization(f,bounds,n, phip, phig, omega, tol, map, num_iterations):
-    paths, particle_val, velocity, local_best, global_best, local_best_val = initialization(f, bounds, n, map)
+# num_iterations = max number of iterations
+# map_dim = length of the map border
+# num_points = number of points forming a path
+def particle_swarm_optimization(f,bounds,n, phip, phig, omega_min, omega_max, tol, map, num_iterations, map_dim, num_points):
 
-    old_global_best_val = 100
+    paths, particle_val, velocity, local_best, global_best, local_best_val = initialization(f, bounds, n, map, map_dim, num_points)
+
+    old_global_best_val = map_dim * num_points
     global_best_val = f(global_best)
     iteration = 1
 
-    iterations_no_improv = 0
+    iterations_no_improv = 1
     found_new_global_best = False
 
     plt.ion()
@@ -146,22 +149,16 @@ def particle_swarm_optimization(f,bounds,n, phip, phig, omega, tol, map, num_ite
         l.append(ax.plot([],[])[0])
     
     # loop for pso iterations
-    while abs(global_best_val - old_global_best_val) > tol  and iteration < num_iterations and iterations_no_improv < 30 :
+    while abs(global_best_val - old_global_best_val) > tol  and iteration < num_iterations and iterations_no_improv < 50 :
 
-
-        print("iteration num "+str(iteration))
+        print("iteration num "+str(iteration)+"      current best path length = "+str(global_best_val))
 
         found_new_global_best = False
 
         # every 30 iterations and if the global best doesn't improve for some iterations we assign random velocity values
-        if iteration%30 == 0:
+        if iteration%30 == 0 or iterations_no_improv%14 == 0:
             for j in range(n):
                 velocity[j] = [rnd.uniform(-abs(bounds[i][1]-bounds[i][0]),abs(bounds[i][1]-bounds[i][0]),2) for i in range(len(bounds))]
-        
-        if(iterations_no_improv%15 == 0):
-            for j in range(n):
-                velocity[j] = [rnd.uniform(-abs(bounds[i][1]-bounds[i][0]),abs(bounds[i][1]-bounds[i][0]),2) for i in range(len(bounds))]
-        
 
         # loop to update every particle/path
         for p in range(n):
@@ -169,6 +166,9 @@ def particle_swarm_optimization(f,bounds,n, phip, phig, omega, tol, map, num_ite
 
             while reset:
                 reset = False
+
+                # adaptive inertia weight to improve exploration and exploitation
+                omega = omega_max - (((omega_max-omega_min)*i)/num_iterations)
 
                 # compute the new particle velocity and position
                 rp = rnd.uniform(0,1)
@@ -185,34 +185,35 @@ def particle_swarm_optimization(f,bounds,n, phip, phig, omega, tol, map, num_ite
                 checkBounds(bounds,paths[p])
 
                 # check if the path crosses an obstacle and generate new random points
-                num_tries = 0
-                while (not checkSegment(np.array([0,0]),paths[p][0]) or map[paths[p][0][0],paths[p][0][1]] == 255) and num_tries<20:
-                    num_tries += 1
+                while (not checkSegment(np.array([0,0]),paths[p][0]) or map[paths[p][0][0],paths[p][0][1]] == 255):
                     paths[p][0] = getRandomInt(bounds[0])
 
-                if num_tries >= 50:
-                    reset = True
-
-                for i in range(1,len(bounds)-1):
+                i = 1
+                while i < num_points-1 and not reset:
                     num_tries = 0
                     while (not checkSegment(paths[p][i-1],paths[p][i]) or map[paths[p][i][0],paths[p][i][1]] == 255) and num_tries<20:
                         num_tries += 1
                         paths[p][i] = getRandomInt(bounds[i])
                     
-                    if num_tries >= 50:
+                    if num_tries >= 20:
                         reset = True
 
-                num_tries = 0
-                while (not checkSegment(paths[p][-1],np.array([map_dimension-1,map_dimension-1])) or \
-                    not checkSegment(paths[p][-2],paths[p][-1]) or map[paths[p][-1][0],paths[p][-1][1]] == 255) and num_tries<20:
-                    paths[p][-1] = getRandomInt(bounds[-1])
-                
-                if num_tries >= 50:
+                    i += 1
+
+                if not reset:
+                    num_tries = 0
+                    while (not checkSegment(paths[p][-1],np.array([map_dimension-1,map_dimension-1])) or \
+                        not checkSegment(paths[p][-2],paths[p][-1]) or map[paths[p][-1][0],paths[p][-1][1]] == 255) and num_tries<20:
+                        num_tries += 1
+                        paths[p][-1] = getRandomInt(bounds[-1])
+                    
+                    if num_tries >= 20:
                         reset = True
 
                 
             particle_val[p] = f(paths[p])
 
+            # update local and global bests
             if particle_val[p] < local_best_val[p]:
                 local_best[p] = paths[p]
                 local_best_val[p] = f(local_best[p])
@@ -227,10 +228,11 @@ def particle_swarm_optimization(f,bounds,n, phip, phig, omega, tol, map, num_ite
         iteration += 1
         
         if found_new_global_best:
-            iterations_no_improv = 0
+            iterations_no_improv = 1
         else:
             iterations_no_improv += 1
 
+        # plot the paths
         for i in range(n):
             path = np.array(local_best[i])
             x,y = path.T
@@ -260,45 +262,41 @@ def manhattan_distance(x):
     total_distance += x[0][0]+x[0][1]
     total_distance += (map_dimension-x[-1][0]) + (map_dimension-x[-1][1])
     for i in range(len(x)-1):
-        total_distance += abs(x[i+1,0]-x[i,0]) + abs(x[i+1,1]-x[i,1])
+        total_distance += abs(x[i+1][0]-x[i][0]) + abs(x[i+1][1]-x[i][1])
 
     return total_distance
 
 
+# problem parameters
 map_dimension = 20
-num_obstacles = 10
+num_obstacles = 5
 map = PV.generate_map(num_obstacles,map_dimension)
 starting_point = (0,0)
 goal = (map_dimension-1,map_dimension-1)
-checkpoint_num = 5
-# ricerca nello spazio dei punti (numero fisso) facenti parte del percorso, fitness = distance
-
+checkpoint_num = 3
 f = euclidean_distance
 dimension_bounds=[0,map_dimension-1]
 bounds=[0]*checkpoint_num
 for i in range(checkpoint_num):
     bounds[i]=dimension_bounds
-p=10 # num of particles 
-num_iterations = 100
+p=10 # num of particles/paths
+num_iterations = 500
 
-#phip=5.8
-#phig=1.3 
-#omega = 0.1
-
+#PSO parameters
 phip = 1.1
-phig = 3.2 
-omega = 0.7
+phig = 6.2 
+#omega = 0.7
+omega_min = 0.7
+omega_max = 1.1
 
 tol=0.0000001
 
 
 
-global_best, global_best_val = particle_swarm_optimization(f, bounds, p, phip, phig, omega, tol, map, num_iterations)
+global_best, global_best_val = particle_swarm_optimization(f, bounds, p, phip, phig, omega_min, omega_max, tol, map, num_iterations, map_dimension, checkpoint_num)
 
-print("global best at "+str(global_best)+ " = "+str(global_best_val))
-
-
+print("global best path = "+str(global_best))
+print("best path length = "+str(global_best_val))
 #PV.drawPath(global_best,map,map_dimension)
 PV.showPath(global_best,map,map_dimension)
-
 plt.show(block=True)
